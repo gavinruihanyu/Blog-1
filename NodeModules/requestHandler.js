451566d366f47374948d4a554bd2cd8654fd0f71
Gavin Yu <gxvinyu@gmail.com>
2021-05-15 23:05:26 -0600

    Simple html pages added, about to add express.

--- NodeModules/requestHandler.js
+++ NodeModules/requestHandler.js
@@ -1,8 +1,26 @@
+const fs = require("fs");
+const homePage = fs.readFileSync("../Public/index.html")
+const aboutPage = fs.readFileSync("../Public/about.html")
+const contactPage = fs.readFileSync("../Public/contact.html")
+
+
 let handles = {
     start: (res, postData) => {
         console.log(`Start has been called`);
-        res.end("Hello Node!");
+        res.writeHead(200, { "Content-Type": `text/html` });
+        res.end(homePage);
+    },
+    about: (res, postData) => {
+        console.log(`Start has been called`);
+        res.writeHead(200, { "Content-Type": `text/html` });
+        res.end(aboutPage);
+    },
+    contact: (res, postData) => {
+        console.log(`Start has been called`);
+        res.writeHead(200, { "Content-Type": `text/html` });
+        res.end(contactPage);
     }
 }
+
 
 exports.handles = handles;
--- NodeModules/router.js
+++ NodeModules/router.js
@@ -1,21 +1,26 @@
+const fs = require("fs");
+const notFoundPage = fs.readFileSync("../Public/404.html");
+
 route = (res, handler, pathName, postData) => {
 
     handle = handler.handles;
 
     handle["/"] = handle.start;
-    handle["/start"] = handle.start;
     //console.log(handle)
 
-    if (!handle[pathName]) {
-       console.log(`No manipulator found for ${pathName}`);
-       res.writeHead(200, { 'Content-Type': 'text/html' });
-       res.write("404 Not Found!");
-       res.end();
+    if (handle[pathName.substring(1)]) {
+        console.log(`About to route a response for ${pathName}`);
+        handle[pathName.substring(1)](res, postData);
     }
-
-    if (handle[pathName]) {
+    else if (handle[pathName]) {
         console.log(`About to route a response for ${pathName}`);
         handle[pathName](res, postData);
+    }
+    else {
+        console.log(`No manipulator found for ${pathName}`);
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(notFoundPage);
+        res.end();
     }
 
 }
